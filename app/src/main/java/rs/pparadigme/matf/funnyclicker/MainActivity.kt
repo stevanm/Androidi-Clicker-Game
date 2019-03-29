b99167d34cf8ed1a83e399a296fad52f0fc09bb6
Stevan Milic <stevan.milic@enetelsolutions.com>
2019-03-29 01:41:54 +0100

    [NF] Init project

---
+++ app/src/main/java/rs/pparadigme/matf/funnyclicker/MainActivity.kt
@@ -0,0 +1,12 @@
+package rs.pparadigme.matf.funnyclicker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
